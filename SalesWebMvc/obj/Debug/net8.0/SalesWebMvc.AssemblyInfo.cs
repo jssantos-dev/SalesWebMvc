f2a12c841e1a458b184fab96a06ec0126daf5075
jssantos-dev <jd.santos7206@gmail.com>
2024-09-15 08:07:29 +0900

    Changing theme

--- SalesWebMvc/obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
+++ SalesWebMvc/obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e83c56d5cad4266f29f0bba495be565731a410e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b6ee5b04d308278e8e6eed31c2abdcbe0e714bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
