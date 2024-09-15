e82b45e9ef5c61351af0bb774933b1f2ff9f64f5
jssantos-dev <jd.santos7206@gmail.com>
2024-09-15 15:38:48 +0900

    Foreign key not null (referential integrity)

--- SalesWebMvc/obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
+++ SalesWebMvc/obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88f8ffb96ace35ca1a0cc5419d4d23d2ef7a1069")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc7807d169fa515312dd50d0c87cad0b161aa6c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
