78319556f4e6e1f34323040d98e5bc9dcc3b22bd
jssantos-dev <jd.santos7206@gmail.com>
2024-09-17 21:15:41 +0900

    Implementing simple search

--- SalesWebMvc/obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
+++ SalesWebMvc/obj/Debug/net8.0/SalesWebMvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ed420405b4efaa32cda7d6ef8dfff464e1a23c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b453033fc278b6824dc8308b00ac1be3ff1d16ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesWebMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
